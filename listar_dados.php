de5fb0421cd824a9a9a3b1fd50470f28b9dc8ca7
JM7087 <joaomarcos@jm7087.com>
2024-03-11 19:49:53 -0300

    ordenar por ordem decrescente dos pontos

--- listar_dados.php
+++ listar_dados.php
@@ -3,7 +3,7 @@
 require_once 'conexao.php';
 
 // Selecionar todos os jogadores do banco
-$stmt = $pdo->query("SELECT * FROM jogadores");
+$stmt = $pdo->query("SELECT * FROM jogadores ORDER BY pontos DESC");
 $jogadores = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // Retornar os jogadores em formato JSON
