f353e9430ba9d213059f09b213156786f9429233
João Marcos de Oliveira santos <joaomarcos@jm7087.com>
2024-03-12 10:10:18 -0300

    limitar o resultado aos 10 jogadores com maior pontuação

--- listar_dados.php
+++ listar_dados.php
@@ -2,8 +2,8 @@
 
 require_once 'conexao.php';
 
-// Selecionar todos os jogadores do banco
-$stmt = $pdo->query("SELECT * FROM jogadores ORDER BY pontos DESC");
+// Selecionar todos os 10 melheres jogadores do banco
+$stmt = $pdo->query("SELECT * FROM jogadores ORDER BY pontos DESC LIMIT 10");
 $jogadores = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // Retornar os jogadores em formato JSON
